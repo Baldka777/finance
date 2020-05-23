76bfb198167b37d5c4cfcb5e707ec14c239ae340
Baldka777 <mik.baldka@gmail.com>
2020-05-23 15:33:02 +0800

    Finance module ruu item add hiideg bolgon

--- app.js
+++ app.js
@@ -22,38 +22,59 @@ var uiController = (function () {
 })();
 // Санхүүтэй ажиллах контроллер
 var financeController = (function () {
+  // private data
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // private data
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  // private data
   var data = {
-    allItems: {
-      inc: [];
-      exp: [];
+    items: {
+      inc: [],
+      exp: [],
     },
-  
-    totals: {
-      inc: 0;
-      exp: 0;
-    }
-  }
 
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+
+  return {
+    addItem: function (type, description, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        data.items[type][data.items[type].length - 1].id + 1;
+      }
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        // type === exp
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+  };
 })();
 
 // Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
     // 2. Олж авсан өгөдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
+
     // 3. Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэн дээр гаргана.
