cbd20fa2135c3fa3de5d6eec0d372bdb7a3d3918
Baldka777 <mik.baldka@gmail.com>
2020-05-23 12:47:49 +0800

    Event listene

---
+++ app.js
@@ -0,0 +1,27 @@
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function () {})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function () {})();
+
+// Програмын холбогч контроллер
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
+    console.log();
+    // 2. Олж авсан өгөдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    // 3. Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэн дээр гаргана.
+  };
+
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
